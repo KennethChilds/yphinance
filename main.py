01152beb6f29d9fb3c5df5f93421335fbbdfc7cf
Kenny Childs <kennychilds@laptop.local>
2024-04-16 12:44:12 -0400

    baba example, edited prompt

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 import ollama
 import yfinance as yf
 
-msft = yf.Ticker("MSFT")
+stocks = yf.Ticker("baba")
 
-data = yf.download("MSFT", period="1mo")
+data = stocks.info
+
     
 # streaming ai respone w/ dolphin-phi
 stream = ollama.chat(
     model='dolphin-phi',
     messages=[{'role': 'user', 'content': f"""
                based on the info in {data}, 
-               what is the general trend of the price?"""}],
+               what is the general trend 
+               of the stock, and is it a 
+               good buy?
+               
+               give absolute advice, give objectiv
+               answers. there is a correct answer
+               give it."""}],
     stream=True,
 )
-print(data)
+print(data, "\n")
 for chunk in stream:
   print(chunk['message']['content'], end='', flush=True)
