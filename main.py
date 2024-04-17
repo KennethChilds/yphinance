d861e81b9bffb0e14dd19416df1215084130f9af
Kenny Childs <kennychilds@laptop.local>
2024-04-17 12:00:41 -0400

    minor tweaks

--- main.py
+++ main.py
@@ -2,9 +2,7 @@ import ollama
 import yfinance as yf
 
 stocks = yf.Ticker("baba")
-
 data = stocks.info
-
     
 # streaming ai respone w/ dolphin-phi
 stream = ollama.chat(
@@ -15,7 +13,7 @@ stream = ollama.chat(
                of the stock, and is it a 
                good buy?
                
-               give absolute advice, give objectiv
+               give absolute advice, give objective
                answers. there is a correct answer
                give it."""}],
     stream=True,
